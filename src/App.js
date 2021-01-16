19a40768ccd6c955f4d4e6b04313d01d55a53603
CharlyRamirez <pingestudio@gmail.com>
2021-01-16 11:14:21 -0600

    Paginas creadas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+// Components
+import {
+    Home,
+    Contact
+} from './Pages';
+import {
+    Admin,
+    SignIn
+} from './Pages/Admin';
+
+// CSS's
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-        <h1>Encuesta</h1>
-    </div>
+      <div>
+        <h1>Estamos en App</h1>
+        <Admin />
+        <SignIn />
+        <Home />
+        <Contact />
+      </div>
   );
 }
 
---
+++ src/Pages/Admin/SignIn.js
@@ -0,0 +1,7 @@
+function SignIn() {
+    return(
+        <h2>SignIn</h2>
+    );
+}
+
+export default SignIn;
