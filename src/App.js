880223f9deacc2f3666381099905008e9821cc8e
CharlyRamirez <pingestudio@gmail.com>
2021-01-16 12:52:26 -0600

    Layout Admin basico listo

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // Routes
 import routes from './config/routes';
 
-// CSS's
-import './App.css';
-
 function App() {
   return (
       <Router>
--- src/Layouts/LayoutAdmin.js
+++ src/Layouts/LayoutAdmin.js
@@ -1,16 +1,37 @@
+import { Route } from 'react-router-dom';
+
+import './LayoutAdmin.scss';
+
 // AntD
 import { Layout } from 'antd';
 
-function LayoutAdmin() {
+function LayoutAdmin({routes}) {
+    const { Header, Content, Footer } = Layout;
     return(
         <Layout>
             <h2>Menu Sider Admin</h2>
-            <div>
-                Contenido...
-            </div>
-            <h5>Footer</h5>
+            <Layout>
+                <Header>Header</Header>
+                <Content>
+                    <LoadRoutes routes={routes} />
+                </Content>
+                <Footer>
+                    Encuestas Hola Mundo
+                </Footer>
+            </Layout>
         </Layout>
     );
+}
+
+function LoadRoutes({routes}) {
+    return routes.map((route, index) => (
+        <Route
+            key={index}
+            path={route.path}
+            exact={route.exact}
+            component={route.component}
+        />
+    ));
 }
 
 export default LayoutAdmin;
