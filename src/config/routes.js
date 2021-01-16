0545b86cc2218b6079f597d1a1ce4bde1a04bb12
CharlyRamirez <pingestudio@gmail.com>
2021-01-16 13:19:00 -0600

    Agregando rutas para usuarios bàsicos

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,8 +1,11 @@
 // Layouts
-import { LayoutAdmin } from "../Layouts";
+import { LayoutAdmin, LayoutBasic } from '../Layouts';
 
 // Admin Pages
-import { Admin as AdminHome, SignIn as AdminSignIn } from "../Pages/Admin";
+import { Admin as AdminHome, SignIn as AdminSignIn } from '../Pages/Admin';
+
+// Basic Pages
+import { Home, Contact } from '../Pages';
 
 const routes = [
     {
@@ -18,6 +21,23 @@ const routes = [
             {
                 path: '/admin/login',
                 component: AdminSignIn,
+                exact: true
+            }
+        ]
+    },
+    {
+        path: '/',
+        component: LayoutBasic,
+        exact: false,
+        routes: [
+            {
+                path: '/',
+                component: Home,
+                exact: true
+            },
+            {
+                path: '/contact',
+                component: Contact,
                 exact: true
             }
         ]
