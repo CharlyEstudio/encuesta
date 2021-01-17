e6c1358b7fc4233df9b2dd228424ba09a5eec14b
CharlyRamirez <pingestudio@gmail.com>
2021-01-16 20:36:48 -0600

    Estilos LoginForm listo

--- src/Pages/Admin/components/LoginForm/LoginForm.js
+++ src/Pages/Admin/components/LoginForm/LoginForm.js
@@ -1,10 +1,36 @@
+// AntD
+import { Form, Input, Button, notification } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+
 import './LoginForm.scss';
 
 function Loginform() {
     return(
-        <div>
-            <h2>LoginForm</h2>
-        </div>
+        <Form className="login-form">
+            <Form.Item>
+                <Input
+                    prefix={<UserOutlined style={{ color: "rgba(0, 0, 0, 0.25)" }} />}
+                    type="email"
+                    name="email"
+                    placeholder="Correo Electrónico"
+                    className="login-form__input"
+                />
+            </Form.Item>
+            <Form.Item>
+                <Input
+                    prefix={<LockOutlined style={{ color: "rgba(0, 0, 0, 0.25)" }}/>}
+                    type="password"
+                    name="password"
+                    placeholder="Contraseña"
+                    className="login-form__input"
+                />
+            </Form.Item>
+            <Form.Item>
+                <Button htmlType="submit" className="login-form__button">
+                    Iniciar Sesión
+                </Button>
+            </Form.Item>
+        </Form>
     );
 }
 
